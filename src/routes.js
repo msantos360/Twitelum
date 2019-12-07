8f707375e9b36c6f6825121a878840ee87db2508
msantos360 <michael.shel96@gmail.com>
2019-12-07 16:51:56 -0300

    Capitulo 15 pag 55

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,57 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom"
-
+import { Switch, Route, Redirect } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 
-class Roteamento extends React.Component{
+const PrivateRoute = ({ component, Component, ...props}) => {
+    const isUserAuthenticated = () => Boolean(localStorage.getItem('TOKEN'))
+
+    if (isUserAuthenticated()){
+        return <Component {...props}/>
+    }
+
+    return <Redirect to="/login" />
+}
+
+const Roteamento = () => {
+    return(
+        <Switch>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/login" component={LoginPage} />
+            <Route component={NotFoundPage} />
+        </Switch>
+    )
+}
+
+/*class PrivateRoute extends React.Component{
+
+    estaAutenticado = () =>{
+        if(localStorage.getItem('TOKEN')){
+            return true
+        }
+        return false
+    }
+    
     render(){
+        
+        const { component: Component, ...props } = this.props
+    
+        if(this.estaAutenticado()){
+            return <Component {...props} />
+        }
+        else{
+            return <Redirect to="/login" />
+        }
+}
+
+class Roteamento extends React.Component{        
         return (
             <Switch>
-                <Route path="/" component={HomePage} exact />
+                <Route path="/" component={Home} exact />
                 <Route path="/login" component={LoginPage} />
             </Switch>
         );
     }
-}
+}*/
 
 export default Roteamento;
